b51e9fe5b3974ff4630d0db7934a776820254e1b
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-30 23:50:28 -0700

    kitchen db

--- souschef.go
+++ souschef.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -11,18 +12,41 @@ import (
 )
 
 var db *sql.DB = nil
+var kdb *sql.DB = nil
 
-func checkQuantity(table string) int {
+func bistroConnect() {
+	var err error = nil
+	db, err = sql.Open("mysql",
+		"root:pizza@tcp(bistrofridge.deterlab.net:3306)/bistrofridge")
+	if err != nil {
+		fmt.Println("error opening connection to bistrofridge")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func kitchenConnect() {
+	var err error = nil
+	kdb, err = sql.Open("postgres",
+		"host=kitchen.deterlab.net user=postgres dbname=fridge sslmode=disable password=pizza")
+	if err != nil {
+		fmt.Errorf("could not connect to the kitchen db")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func checkQuantity(table, dbname string, db *sql.DB) int {
 
 	q := fmt.Sprintf("SELECT count FROM %s", table)
 	rows, err := db.Query(q)
 	if err != nil {
-		fmt.Println("unable to query bistrofridge")
+		fmt.Println("unable to query ", dbname)
 		fmt.Println(err)
 	}
 
 	if !rows.Next() {
-		fmt.Println("the bistrofridge is broken :(")
+		fmt.Println("the ", dbname, " is broken :(")
 		rows.Close()
 		return -1
 	}
@@ -30,7 +54,7 @@ func checkQuantity(table string) int {
 	var count int
 	err = rows.Scan(&count)
 	if err != nil {
-		fmt.Println("the bistrofridge is empty!")
+		fmt.Println("the ", dbname, " is empty!")
 		fmt.Println(err)
 	}
 
@@ -50,22 +74,38 @@ func restock(table string, count int) {
 	}
 }
 
+func grabIngredientPacks(count int) error {
+	err := kdb.Ping()
+	if err != nil {
+		fmt.Println("unable to ping kitchen")
+		fmt.Println(err)
+
+		fmt.Println("trying to reconnect")
+		kitchenConnect()
+	}
+
+	fmt.Println("grabbing fresh ingredients for the bistro from the kitchen fridge")
+	count = checkQuantity("ingredient_packs", "kitchen fridge", kdb)
+	cmd := fmt.Sprintf("UPDATE ingredient_packs SET count = %d", count-200)
+	_, err = kdb.Query(cmd)
+	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf("the kitchen fridge is broke :/")
+	}
+
+	return nil
+}
+
 func main() {
 
 	fmt.Println("I am the souschef har de har har")
 
-	var err error = nil
-	db, err = sql.Open("mysql",
-		"root:pizza@tcp(bistrofridge.deterlab.net:3306)/bistrofridge")
-	if err != nil {
-		fmt.Println("error opening connection to bistrofridge")
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	bistroConnect()
+	kitchenConnect()
 
 	for {
 
-		err = db.Ping()
+		err := db.Ping()
 		if err != nil {
 			fmt.Println("unable to ping bistrofridge")
 			fmt.Println(err)
@@ -80,11 +120,12 @@ func main() {
 			}
 		}
 
-		if q := checkQuantity("ingredient_packs"); q < 100 {
+		if q := checkQuantity("ingredient_packs", "bistrofridge", db); q < 100 {
+			grabIngredientPacks(200)
 			restock("ingredient_packs", q)
 		}
 
-		if q := checkQuantity("doughballs"); q < 100 {
+		if q := checkQuantity("doughballs", "bistrofridge", db); q < 100 {
 			resp, err := http.Get("http://mixer.deterlab.net:8085/mix")
 			if err != nil {
 				fmt.Println("unable to make dough :(")
