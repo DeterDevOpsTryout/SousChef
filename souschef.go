68d17815839a8b05545b094dce99f8edce4bae99
Ryan Goodfellow <rgoodfel@isi.edu>
2015-09-30 23:11:04 -0700

    talking to mixer

--- souschef.go
+++ souschef.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"time"
 )
@@ -21,6 +23,8 @@ func checkQuantity(table string) int {
 
 	if !rows.Next() {
 		fmt.Println("the bistrofridge is broken :(")
+		rows.Close()
+		return -1
 	}
 
 	var count int
@@ -29,6 +33,8 @@ func checkQuantity(table string) int {
 		fmt.Println("the bistrofridge is empty!")
 		fmt.Println(err)
 	}
+
+	rows.Close()
 
 	return count
 
@@ -49,20 +55,29 @@ func main() {
 	fmt.Println("I am the souschef har de har har")
 
 	var err error = nil
+	db, err = sql.Open("mysql",
+		"root:pizza@tcp(bistrofridge.deterlab.net:3306)/bistrofridge")
+	if err != nil {
+		fmt.Println("error opening connection to bistrofridge")
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	for {
-
-		db, err = sql.Open("mysql", "root:pizza@tcp(bistrofridge.deterlab.net:3306)/bistrofridge")
-		if err != nil {
-			fmt.Println("error opening connection to bistrofridge")
-			fmt.Println(err)
-			os.Exit(1)
-		}
 
 		err = db.Ping()
 		if err != nil {
 			fmt.Println("unable to ping bistrofridge")
 			fmt.Println(err)
+
+			fmt.Println("trying to reconnect")
+			db, err = sql.Open("mysql",
+				"root:pizza@tcp(bistrofridge.deterlab.net:3306)/bistrofridge")
+			if err != nil {
+				fmt.Println("error opening connection to bistrofridge")
+				fmt.Println(err)
+				os.Exit(1)
+			}
 		}
 
 		if q := checkQuantity("ingredient_packs"); q < 100 {
@@ -70,7 +85,22 @@ func main() {
 		}
 
 		if q := checkQuantity("doughballs"); q < 100 {
-			restock("doughballs", q)
+			resp, err := http.Get("http://mixer.deterlab.net:8085/mix")
+			if err != nil {
+				fmt.Println("unable to make dough :(")
+				fmt.Println(err)
+			} else {
+				body, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					fmt.Println("the dough has bugs")
+					fmt.Println(err)
+				} else if string(body) != "dough" {
+					fmt.Println("the dough is not dough")
+					fmt.Println(err)
+				} else {
+					restock("doughballs", q)
+				}
+			}
 		}
 
 		time.Sleep(2 * time.Second)
